9798dbfe47e41c4ebd2b9292de6384c7176bb031
KenMusembi <kenmusembi21@gmail.com>
2022-01-30 16:26:52 +0300

    setup instructions on readme

--- README.md
+++ README.md
@@ -1,3 +1,63 @@
 # Mojagate SMS API Integration
 
 This is a Laravel integration with the Mojagate SMS API.
+
+## Prerequisite
+PHP 8.1.0
+Laravel 8.81.0
+Composer 2.2.5
+
+## Installation
+To start, clone the repository and go the project folder
+`git clone https://github.com/KenMusembi/sms_integrations.git`
+
+`cd mojagate`
+
+install composer and all the prooject dependancies
+ `composer install` 
+
+generate the laravel key
+`php artisan key:generate`
+
+copy env file and setup DB and custom mojagate credentials
+`.env.example` to `.env`
+
+run npm for the node packages
+`npm`, `npm run dev`
+
+run migrations
+`php artisan migrate`
+
+start the application!
+`php artisan serve`
+
+go to http://127.0.0.1:8000/register and register as a new user, you will see a table of message transactions, there will be none, so let us send our first message.
+
+## Login to use the send sms api
+use a api platform (like postman, here is a link to the collection https://www.getpostman.com/collections/a902c0d1e954d5f726c5), 
+go to http://127.0.0.1:8000/api/V1/login in the body, put the credentials in json format:
+email:kenmusembi21@gmaill.com
+password:12345678
+
+send as post request with the data
+```bash
+{
+    "email":"kenmusembi21@gmail.com",
+    "password":"12345678"
+}
+```
+
+send as a post api and you will get a token which we will use in the next step, so save it.
+
+## Send SMS API
+then go to http://127.0.0.1:8000/api/V1/sendsms, which is also a post api.
+
+add the token given in the previous step as Bearer {token} and then indicate the recipient and message in the body segment, like so:
+```bash
+{
+    "recipient":"254748050434",
+    "message":"Hello, This is a test SMS"
+}
+```
+
+You will get a 201 response with a success message!
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,51 +2,51 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 @section('content')
 @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    @if(session()->has('success'))
-    <div class="alert alert-success text-center">
-         {{ session()->get('success') }}
-    </div>
+<div class="alert alert-success" role="alert">
+  {{ session('status') }}
+</div>
+@endif
+@if(session()->has('success'))
+<div class="alert alert-success text-center">
+  {{ session()->get('success') }}
+</div>
 @endif
 
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                  
+        <div class="card-body">
 
-                   
-<div class="container">
-  <h2>SMS Message History</h2>
 
-<table class="table">
-<tr>
-<td>Id</td>
-<td>First Name</td>
-<td>Last Name</td>
-<td>City Name</td>
-<td>Date</td>
-</tr>
-@foreach ($records as $record)
-<tr>
-<td>{{ $record->id }}</td>
-<td>{{ $record->status }}</td>
-<td>{{ $record->recipient }}</td>
-<td>{{ $record->message }}</td>
-<td>{{ $record->date_sent }}</td>
-</tr>
-@endforeach
-</table>
 
-                </div>
-            </div>
+          <div class="container">
+            <h2>SMS Message History</h2>
+
+            <table class="table">
+              <tr>
+                <td>Id</td>
+                <td>First Name</td>
+                <td>Last Name</td>
+                <td>City Name</td>
+                <td>Date</td>
+              </tr>
+              @foreach ($records as $record)
+              <tr>
+                <td>{{ $record->id }}</td>
+                <td>{{ $record->status }}</td>
+                <td>{{ $record->recipient }}</td>
+                <td>{{ $record->message }}</td>
+                <td>{{ $record->date_sent }}</td>
+              </tr>
+              @endforeach
+            </table>
+
+          </div>
         </div>
+      </div>
     </div>
-</div>
-@endsection
+  </div>
+  @endsection
